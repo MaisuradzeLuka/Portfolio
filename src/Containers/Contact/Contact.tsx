74c8d9a40b7d755ff6b04e8ef29cc553a58c1bf1
MaisuradzeLuka <lukamaisuradze1218203@gmail.com>
2024-01-07 20:54:37 +0400

    adds form styling

--- src/Containers/Contact/Contact.tsx
+++ src/Containers/Contact/Contact.tsx
@@ -1,9 +1,40 @@
+import { Form, Formik } from "formik";
 import AppWrap from "../../Wrapper/AppWrap";
 
 import "./Contact.scss";
 
+interface IInitialValues {
+  name: string;
+  email: string;
+  msg: string;
+}
+
+const initialValues: IInitialValues = {
+  name: "",
+  email: "",
+  msg: "",
+};
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const submitHandler = () => {};
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={submitHandler}>
+      {() => (
+        <Form className="contact">
+          <h2>Contact Me</h2>
+          <div>
+            <input type="text" placeholder="Your Name" name="name" />
+            <input placeholder="Your Email" type="email" name="email" />
+            <textarea placeholder="Your Message" name="msg" />
+          </div>
+          <button type="submit" className="submitBtn">
+            Send Message
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default AppWrap(Contact, "contact");
